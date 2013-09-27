38500b14c22ca9310823b3ba622f684f723fac63
Brandon Wulf <bwulf@labattfood.com>
2013-09-27 13:43:08 -0500

    Add float indicator per channel. Correctly look only at child nodes.

--- MultiValueXML/MultiValueXML.vb
+++ MultiValueXML/MultiValueXML.vb
@@ -9,18 +9,21 @@ Imports Gnu.Getopt
 Module MultiValueXML
 
     Sub Main(ByVal cmdArgs() As String)
-        Dim url As String = "http://172.16.12.92:8081/mbean?objectname=org.apache.cassandra.request:type=ReadStage&template=identity"
+        Dim url As String = "http://172.16.12.92:8081/mbean?objectname=org.apache.cassandra.db:type=ColumnFamilies,keyspace=commons,columnfamily=items_&template=identity"
         'Dim xmlReader As XmlTextReader = New XmlTextReader("http://" & HttpUtility.UrlEncode(url))
-        Dim xpath As String = "/MBean/Attribute"
-        Dim key As String = "@name"
-        Dim value As String = "@value"
-
+        Dim x_xpath As String = "/MBean/Attribute[contains(@availability,'R')]"
+        Dim x_key As String = "//@name"
+        Dim x_value As String = "//@value"
+        Dim x_type As String = "//@type"
+        Dim a_floaters As String = "long,double"
 
         Dim LongOptions() As LongOpt = {
             New LongOpt("url", Argument.Required, Nothing, Asc("u")),
             New LongOpt("xpath", Argument.Required, Nothing, Asc("x")),
             New LongOpt("key", Argument.Required, Nothing, Asc("k")),
             New LongOpt("value", Argument.Required, Nothing, Asc("v")),
+            New LongOpt("type", Argument.Required, Nothing, Asc("t")),
+            New LongOpt("float", Argument.Required, Nothing, Asc("f")),
             New LongOpt("help", Argument.No, Nothing, Asc("?"))
             }
         Dim g As New Getopt("MultiValueXML.exe", cmdArgs, Getopt.digest(LongOptions), LongOptions)
@@ -33,11 +36,15 @@ Module MultiValueXML
                 Case Asc("u")
                     url = g.Optarg()
                 Case Asc("x")
-                    xpath = g.Optarg()
+                    x_xpath = g.Optarg()
                 Case Asc("k")
-                    key = g.Optarg()
+                    x_key = g.Optarg()
                 Case Asc("v")
-                    value = g.Optarg()
+                    x_value = g.Optarg()
+                Case Asc("t")
+                    x_type = g.Optarg()
+                Case Asc("f")
+                    a_floaters = g.Optarg()
                 Case Asc("?")
                     WriteLine("Available Options:")
                     WriteLine(vbNewLine & vbTab & "u, --url")
@@ -48,6 +55,10 @@ Module MultiValueXML
                     WriteLine(vbTab & vbTab & "What to append to XPath to get the key")
                     WriteLine(vbNewLine & vbTab & "v, --value")
                     WriteLine(vbTab & vbTab & "What to append to XPath to get the value")
+                    WriteLine(vbNewLine & vbTab & "t, --type")
+                    WriteLine(vbTab & vbTab & "What to append to XPath to get the type")
+                    WriteLine(vbNewLine & vbTab & "f, --float")
+                    WriteLine(vbTab & vbTab & "Types (returned by --type query) that should be considered float")
 
                     Return
             End Select
@@ -57,15 +68,16 @@ Module MultiValueXML
         Try
             Dim xml As XDocument = XDocument.Load(url)
 
-            For Each node As XElement In xml.XPathSelectElements(xpath)
-                Dim channelName As String = GetValue(node, key)
-                Dim channelValue As String = GetValue(node, value)
-                resultXML.Add(New XElement("result", New XElement("channel", channelName), New XElement("value", channelValue)))
+            For Each node As XElement In xml.XPathSelectElements(x_xpath)
+                Dim channelName As String = GetValue(node, x_key)
+                Dim channelValue As String = GetValue(node, x_value)
+                Dim isFloat As Boolean = GetFloat(node, x_type, a_floaters.Split(","))
+
+                resultXML.Add(New XElement("result", New XElement("channel", channelName), New XElement("value", channelValue), IIf(isFloat, New XElement("float", 1), New XElement("float", 0))))
             Next
 
             If resultXML.Elements.Count = 0 Then
-                resultXML.Add(New XCData("URL:" & url & vbCrLf & "XPath:" & xpath & vbCrLf & "KEY:" & key & vbCrLf & "VALUE:" & value & vbCrLf))
-
+                resultXML.Add(New XCData("URL:" & url & vbCrLf & "XPath:" & x_xpath & vbCrLf & "KEY:" & x_key & vbCrLf & "VALUE:" & x_value & vbCrLf))
             End If
 
         Catch ex As Exception
@@ -81,11 +93,12 @@ Module MultiValueXML
         Return value
     End Function
 
-    Private Function GetValue(node As XElement, xquery As String) As String
+    Private Function GetValue(aNode As XElement, xquery As String) As String
         Dim returnValue As String
-        Dim result As IEnumerable = CType(node.XPathEvaluate(xquery), IEnumerable)
+        Dim newDoc As XDocument = New XDocument(aNode.DescendantNodesAndSelf)
+        Dim result As IEnumerable = CType(newDoc.XPathEvaluate(xquery), IEnumerable)
 
-        If xquery.Substring(Math.Max(0, xquery.LastIndexOf("/")), 1) = "@" Then
+        If xquery.Substring(Math.Max(0, xquery.LastIndexOf("/") + 1), 1) = "@" Then
             returnValue = result.Cast(Of XAttribute).FirstOrDefault.Value.ToString
         Else
             returnValue = result.Cast(Of XElement).FirstOrDefault.Value.ToString
@@ -93,5 +106,20 @@ Module MultiValueXML
 
         Return returnValue
     End Function
+
+    Private Function GetFloat(aNode As XElement, xquery As String, floaters() As String) As Boolean
+        Dim returnValue As String
+        Dim newDoc As XDocument = New XDocument(aNode.DescendantNodesAndSelf)
+        Dim result As IEnumerable = CType(newDoc.XPathEvaluate(xquery), IEnumerable)
+
+        If xquery.Substring(Math.Max(0, xquery.LastIndexOf("/")+1), 1) = "@" Then
+            returnValue = result.Cast(Of XAttribute).FirstOrDefault.Value.ToString
+        Else
+            returnValue = result.Cast(Of XElement).FirstOrDefault.Value.ToString
+        End If
+
+        Return floaters.Contains(returnValue)
+    End Function
+
 
 End Module
